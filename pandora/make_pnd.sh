60ac29625eab9502f3995ba067e74c2243572ba2
Hitnrun <rangelspam@gmail.com>
2010-07-22 22:11:40 +0000

    Pandora PND generation

---
+++ pandora/make_pnd.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#cp ../src/gameandwatch gameandwatch/bin
+#cp ../res/gameandwatch.png gameandwatch/data
+
+pnd_make.sh -p gameandwatch_0.3.pnd -d gameandwatch -x gameandwatch/PXML.xml -i gameandwatch/data/gameandwatch.png
